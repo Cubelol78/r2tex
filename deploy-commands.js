8e761da4de5da4bc4492e03de86b49cf9114c3df
Cubelol78 <99619715+Cubelol78@users.noreply.github.com>
2022-10-08 22:25:10 +0200

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,13 +6,15 @@ const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+require("dotenv").config();
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+//const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
     try {
