c4a663ff92747f53edd265b42251da798ac2e7ba
Cubelol78 <99619715+Cubelol78@users.noreply.github.com>
2022-10-09 15:06:27 +0200

    Add files via upload

--- commands/info_serveur.js
+++ commands/info_serveur.js
@@ -1,6 +1,6 @@
 // info_serveur.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { CommandInteraction, EmbedBuilder, User } = require('discord.js');
+const { CommandInteraction, EmbedBuilder, User, GuildEmoji, Role, Widget } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -30,10 +30,51 @@ module.exports = {
         const FooterURL = 'https://zupimages.net/up/22/03/ws50.jpg'
         const BotJoinGuild = interaction.guild.joinedAt.toLocaleString()
         const BotID = interaction.user.id
+        const GuildEmojisCounter = interaction.guild.emojis.cache.size
+        const GuildRoleList = interaction.guild.roles.cache.map(role => role.toString())
+        const GuildUserName = interaction.user.tag
+        const GuildUserAvatar = interaction.user.avatarURL()
+
+        //==================//
+        //Compteur de Boost//
+        //================//
+        const GuildBoostCounterCalcul = interaction.guild.premiumSubscriptionCount
+        var GuildBoostCounter = ''
+        if ( GuildBoostCounterCalcul == null )
+            GuildBoostCounter = 'Non définis'
+        else
+            GuildBoostCounter = `${GuildBoostCounterCalcul}`
+
+        //=====================================================//
+        //Authentification a double facteur pour la modération//
+        //===================================================//
+        const GuildAdminA2FCalcul = interaction.guild.mfaLevel
+        var GuildAdminA2F = ''
+        if ( GuildAdminA2FCalcul == null )
+            GuildAdminA2F = 'Non définis'
+        else
+            if ( GuildAdminA2FCalcul == 0 )
+                GuildAdminA2F = 'Désactiver'
+            else
+                if ( GuildAdminA2FCalcul == 1 )
+                    GuildAdminA2F = 'Activer'
+
+        //=================//
+        //Liste des Emojis//
+        //===============//
+        const GuildEmojisListCalcul = interaction.guild.emojis.cache.map(emoji => emoji.toString())
+        var GuildEmojisList = ''
+        if ( GuildEmojisListCalcul == null )
+            GuildEmojisList = 'Non définis'
+        else
+            if ( GuildEmojisListCalcul == '' )
+                GuildEmojisList = 'Non définis'
+            else
+                GuildEmojisList = `${GuildEmojisListCalcul}`
         
-        ////
-        ////
-        ////
+        //=============//
+        //Vérification//
+        //===========//
         const GuildVerificationCalcul = interaction.guild.verificationLevel
         var GuildVerification = ''
         if ( GuildVerificationCalcul == null )
@@ -43,16 +84,16 @@ module.exports = {
                 GuildVerification = 'Aucune'
             else
                 if ( GuildVerificationCalcul == 1 )
-                    GuildVerification = 'Faible (Doit avoir une adresse email vérifier sur son compte)'
+                    GuildVerification = 'Faible-(Doit avoir une adresse email vérifier sur son compte)'
                 else
                     if ( GuildVerificationCalcul == 2 )
-                        GuildVerification = 'Moyen (Doit avoir une adresse email vérifier sur son compte et être inscrit depuis plus de 5 minutes)'
+                        GuildVerification = 'Moyen-(Doit avoir une adresse email vérifier sur son compte et être inscrit depuis plus de 5 minutes)'
                     else
                         if ( GuildVerificationCalcul == 3 )
-                            GuildVerification = 'Elevé (Doit avoir une adresse email vérifier sur son compte et avoir rejoint le serveur depuis plus de 10 minutes)'
+                            GuildVerification = 'Elevé-(Doit avoir une adresse email vérifier sur son compte et avoir rejoint le serveur depuis plus de 10 minutes)'
                         else
                             if ( GuildVerificationCalcul == 4 )
-                                GuildVerification = 'Maximum (Doit avoir un numéro de téléphone relier à son compte discord)'
+                                GuildVerification = 'Maximum-(Doit avoir un numéro de téléphone relier à son compte discord)'
 
         //============//
         //Timeout AFK//
@@ -115,6 +156,9 @@ module.exports = {
          else
           if ( GuildAnalyseCalcul == 2 )
               GuildAnalyse = 'Analyse tous les contenus du serveur'
+          else
+            if ( GuildAnalyseCalcul == null )
+                GuildAnalyse = 'Non définis'
 
         //==================================//
         //Affichage de la Bannière de Guild//
@@ -123,6 +167,8 @@ module.exports = {
         var GuildBannerUrl
         if ( GuildBannerUrlCalcul == null )
             GuildBannerUrl = 'Bannière de Guild non définis'
+        else
+            GuildBannerUrl = `${GuildBannerUrlCalcul}`
 
         //============================================//
         //Paramétrage des notification sur le serveur//
@@ -130,10 +176,13 @@ module.exports = {
         const GuildNotificationCalcul = interaction.guild.defaultMessageNotifications
         var GuildNotification = ''
 
-        if ( GuildNotificationCalcul == 0 )
-          GuildNotification = 'Tous les messages'
+        if ( GuildNotificationCalcul == null )
+            GuildNotification = 'Non définis'
         else
-          GuildNotification = '@Mentions seulement'
+            if ( GuildNotificationCalcul == 0 )
+                GuildNotification = 'Tous les messages'
+            else
+                GuildNotification = '@Mentions seulement'
 
         //===================//
         //Embed Info Serveur//
@@ -146,20 +195,23 @@ module.exports = {
             )
         .setURL(`${ServeurIconURL}`)
         .addFields(
-            { name: 'Propriétaire du serveur', value: `**Pseudo:**<@${IDowner}>`, inline: false },
-            { name: 'Description du serveur', value: `${GuildDescription}`, inline: false },
-            { name: 'Membre sur le serveur', value: `${CounterMemberGuild}`, inline: true },
-            { name: 'Nombre de channel', value: `${CounterChannelGuild}`, inline: true },
-            { name: 'Nombre de roles', value: `${RoleCount}`, inline: true },
-            { name: 'Réglement', value: `${GuildRuleChannel}`, inline: true },
-            { name: 'Community Updates', value: `${GuildComUpdate}`, inline: true },
-            { name: 'AFK', value: `**Channel:**${GuildAfkChannel}\n**Timeout:** ${GuildAfkTimeout}`, inline: true },
-            { name: 'Bannière de Guild', value: `${GuildBannerUrl}`, inline: false },
-            { name: 'Paramétre des Notification', value: `${GuildNotification}`, inline: true },
-            { name: 'Filtre des contenus', value: `${GuildAnalyse}`, inline: true },
-            { name: `Vérification`, value: `${GuildVerification}`, inline: false },
-            { name: `[BOT]R2tex a rejoint le`, value: `${BotJoinGuild}`, inline: false },
-            //{ name: `Emojis`, value: `${test}`, inline: false },
+            { name: 'Propriétaire du serveur:', value: `**Pseudo:**<@${IDowner}>`, inline: true },
+            { name: 'Boost:', value: `${GuildBoostCounter}`, inline: true },
+            { name: 'Description du serveur:', value: `${GuildDescription}`, inline: false },
+            { name: 'Membre sur le serveur:', value: `${CounterMemberGuild}`, inline: true },
+            { name: 'Nombre de channel:', value: `${CounterChannelGuild}`, inline: true },
+            //{ name: 'Nombre de roles', value: `${RoleCount}`, inline: true },
+            { name: 'AFK:', value: `**Channel:**${GuildAfkChannel}\n**Timeout:** ${GuildAfkTimeout}`, inline: false },
+            { name: 'Réglement:', value: `${GuildRuleChannel}`, inline: true },
+            { name: 'Community Updates:', value: `${GuildComUpdate}`, inline: true },
+            { name: 'Bannière de Guild:', value: `${GuildBannerUrl}`, inline: false },
+            { name: 'Paramétre des Notification:', value: `${GuildNotification}`, inline: true },
+            { name: 'Filtre des contenus:', value: `${GuildAnalyse}`, inline: true },
+            { name: `Vérification:`, value: `${GuildVerification}`, inline: false },
+            { name: `Authentification a double facteur pour la modération:`, value: `${GuildAdminA2F}`, inline: false },
+            { name: `Rôles [${RoleCount}]:`, value: `${GuildRoleList}`, inline: false },
+            { name: `Emojis [${GuildEmojisCounter}]:`, value: `${GuildEmojisList}`, inline: false },
+            { name: `[BOT]R2tex a rejoint le:`, value: `${BotJoinGuild}`, inline: false },
         )
         //.addField(
         //    'Membre sur le serveur', `${CounterMemberGuild}`, true
@@ -189,8 +241,8 @@ module.exports = {
         ////    "test",`${test}`,false
         ////)
         .setFooter({
-           text: 'Monsieur Propre !!!',
-           iconURL: `${FooterURL}`
+           text: `Demandé(e) par ${GuildUserName}`,
+           iconURL: `${GuildUserAvatar}`
        })
         .setThumbnail(
              ServeurIconURL 
