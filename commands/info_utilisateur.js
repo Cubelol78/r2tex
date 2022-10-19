7bd12facb073748d831e23689f9bf301eae3744b
Cubelol78 <99619715+Cubelol78@users.noreply.github.com>
2022-10-19 12:39:49 +0200

    Add files via upload

--- commands/info_utilisateur.js
+++ commands/info_utilisateur.js
@@ -10,7 +10,7 @@ module.exports = {
             option
                 .setName('user')
                 .setDescription('Recherche un utilisateur')
-                .setRequired(true)
+                .setRequired(false)
         )
         ,
     /**
@@ -19,25 +19,73 @@ module.exports = {
      */
 
     async execute(interaction) {
-        const UserSelect = interaction.options.getUser('user')
-        const UserSelectUserName = UserSelect.tag
-        const UserSelectID = UserSelect.id
-        const UserSelectDateCreate = UserSelect.createdAt.toLocaleDateString()
-        const UserSelectTimeCreate = UserSelect.createdAt.toLocaleTimeString()
+        //=============================================================================================================//
 
+        //===============================//
+        //=====//Constant Général//=====//
+        //=============================//
+        
+        if ( interaction.options.getUser('user') == null )
+            UserSelect = interaction.user
+        else
+            if ( interaction.options.getUser('user') == undefined )
+                UserSelect = interaction.user
+            else
+                if ( interaction.options.getUser('user') == '' )
+                    UserSelect = interaction.user
+                else
+                    UserSelect = interaction.options.getUser('user')
+
+        //const UserSelect = interaction.options.getUser('user')
         const DefaultAvatar = interaction.user.defaultAvatarURL
-        const AvatarUrl = interaction.user.avatarURL()
+
+        //=============================================================================================================//
+
+        //=================================================//
+        //=====//User ou Utilisateur de la commande//=====//
+        //===============================================//
+        const UserAvatarUrlCalcul = interaction.user.avatarURL()
         //const UserId = interaction.user.id
         const UserName = interaction.user.tag
         //const UserDateCreate = interaction.user.createdAt.toLocaleDateString()
         //const UserTimeCreate = interaction.user.createdAt.toLocaleTimeString()
-        //const UserBanner = interaction.user.bannerURL()
+        //const UserBannerCalcul = interaction.user.bannerURL()
         //const FooterURL = 'https://zupimages.net/up/22/03/ws50.jpg'
 
         //===================================//
         //Vérification qu'il y est un avatar//
         //=================================//
+        var UserAvatarUrl = ''
+        if ( UserAvatarUrlCalcul == null )
+            UserAvatarUrl = `${DefaultAvatar}`
+        else
+            if ( UserAvatarUrlCalcul == 'undefined' )
+                UserAvatarUrl = `${DefaultAvatar}`
+            else
+                if ( UserAvatarUrlCalcul == '' )
+                    UserAvatarUrl = `${DefaultAvatar}`
+                else
+                    UserAvatarUrl = `${UserAvatarUrlCalcul}`
+
+        //=============================================================================================================//
+        
+        //=========================//
+        //=====//UserSelect//=====//
+        //=======================//
+
+        //=========//
+        //Constant//
+        //=======//
+        const UserSelectUserName = UserSelect.tag
+        const UserSelectID = UserSelect.id
+        const UserSelectDateCreate = UserSelect.createdAt.toLocaleDateString()
+        const UserSelectTimeCreate = UserSelect.createdAt.toLocaleTimeString()
         const UserSelectAvatarUrlCalcul = UserSelect.avatarURL()
+        //const UserSelectBannerCalcul = UserSelect.bannerURL()
+
+        //===================================//
+        //Vérification qu'il y est un avatar//
+        //=================================//
         var UserSelectAvatarUrl = ''
         if ( UserSelectAvatarUrlCalcul == null )
             UserSelectAvatarUrl = `${DefaultAvatar}`
@@ -45,12 +93,20 @@ module.exports = {
             if ( UserSelectAvatarUrlCalcul == 'undefined' )
                 UserSelectAvatarUrl = `${DefaultAvatar}`
             else
-                UserSelectAvatarUrl = `${UserSelectAvatarUrlCalcul}`
+                if ( UserSelectAvatarUrlCalcul == '' )
+                    UserSelectAvatarUrl = `${DefaultAvatar}`
+                else
+                    UserSelectAvatarUrl = `${UserSelectAvatarUrlCalcul}`
 
-        //=======================//
-        //Bannière d'Utilisateur//
-        //=====================//
-        const UserSelectBannerCalcul = UserSelect.bannerURL()
+        //====================//
+        //Bannière UserSelect//
+        //==================//
+        var UserSelectBannerCalcul = ''
+        if ( UserSelect == 'undefined' )
+            UserSelectBannerCalcul = null
+        else
+            UserSelectBannerCalcul = UserSelect.bannerURL()
+        
         var UserSelectBanner = ''
         if ( UserSelectBannerCalcul == null )
             UserSelectBanner = 'Non définis'
@@ -58,11 +114,16 @@ module.exports = {
             if ( UserSelectBannerCalcul == 'undefined' )
                 UserSelectBanner = 'Non définis'
             else
-                UserSelectBanner = `${UserSelectBannerCalcul}`
+                if ( UserSelectBannerCalcul == '' )
+                    UserSelectBanner = 'Non définis'
+                else
+                    UserSelectBanner = `${UserSelectBannerCalcul}`
+
+        //=============================================================================================================//
         
-        //================//
-        //Embed info user//
-        //==============//
+        //==============================//
+        //=====//Embed Info User//=====//
+        //============================//
         const embedinfouser = new EmbedBuilder()
         .setTitle('Information Utilisateur')
         .setColor('#ff0000')
@@ -73,26 +134,20 @@ module.exports = {
             { name: 'Date de création:', value: `${UserSelectDateCreate}, ${UserSelectTimeCreate}` },
             { name: 'Bannière', value: `${UserSelectBanner}` },
         )
-        //.addField(
-        //    "Nom:",`${UserName}`,false
-        //)
-        //.addField(
-        //    "ID:",`${UserId}`,false
-        //)
-        //.addField(
-        //    "Date de création:",`${UserDateCreate}|${UserTimeCreate}`,false
-        //)
-        //.addField(
-        //    "Bannière",`${UserBanner}`,false
-        //)
         .setFooter({
            text: `Demandé(e) par ${UserName}`,
-           iconURL: `${AvatarUrl}`
+           iconURL: `${UserAvatarUrl}`
        })
         .setThumbnail(
              `${UserSelectAvatarUrl}` 
         )
         .setTimestamp()
+
+        //=============================================================================================================//
+
+        //=====================//
+        //Envois de la réponse//
+        //===================//
         await interaction.reply({ embeds: [embedinfouser], ephemeral: false });
     }
 }
